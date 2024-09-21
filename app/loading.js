78f4adde1edc8e305314059b0fd74cd8f90592dc
emindzanko1 <edzanko1@etf.unsa.ba>
2024-09-21 15:53:38 +0200

    Pinned items connected with backend, added loading indicator

---
+++ app/loading.js
@@ -0,0 +1,5 @@
+import classes from './loading.module.css';
+
+export default function LoadingPage() {
+  return <p className={classes.LoadingPage}>Fetching data...</p>;
+}
--- components/PinnedSidebar/PinnedSection.js
+++ components/PinnedSidebar/PinnedSection.js
@@ -1,77 +1,42 @@
-'use client';
+import { promises as fs } from 'fs';
 import PinnedItem from './PinnedItem';
 
 import classes from './PinnedSection.module.css';
-import { useState } from 'react';
 
-const PinnedSection = () => {
-  const [leagues, setLeagues] = useState([
-    {
-      id: 2021,
-      name: 'Premier League',
-      flag: 'https://upload.wikimedia.org/wikipedia/en/thumb/f/f2/Premier_League_Logo.svg/1920px-Premier_League_Logo.svg.png',
-    },
-    {
-      id: 2014,
-      name: 'La Liga',
-      flag: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/LaLiga_EA_Sports_2023_Vertical_Logo.svg/360px-LaLiga_EA_Sports_2023_Vertical_Logo.svg.png',
-    },
-    {
-      id: 2022,
-      name: 'Premier League',
-      flag: 'https://upload.wikimedia.org/wikipedia/en/thumb/f/f2/Premier_League_Logo.svg/1920px-Premier_League_Logo.svg.png',
-    },
-    {
-      id: 2023,
-      name: 'La Liga',
-      flag: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/LaLiga_EA_Sports_2023_Vertical_Logo.svg/360px-LaLiga_EA_Sports_2023_Vertical_Logo.svg.png',
-    },
-  ]);
-  const [teams, setTeams] = useState([
-    {
-      id: 65,
-      name: 'Manchester City',
-      flag: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/eb/Manchester_City_FC_badge.svg/800px-Manchester_City_FC_badge.svg.png',
-    },
-    { id: 81, name: 'Barcelona', flag: 'https://upload.wikimedia.org/wikipedia/en/4/47/FC_Barcelona_%28crest%29.svg' },
-  ]);
+const PinnedSection = async () => {
+  const leaguesFile = await fs.readFile(process.cwd() + '/lib/data/leagues.json', 'utf8');
+  const leaguesReq = JSON.parse(leaguesFile);
+  const leagues = leaguesReq.leagues;
 
-  const handlePinClick = (e, id, type) => {
-    e.stopPropagation();
-    if (type === 'league') {
-      setLeagues(leagues.filter(league => league.id !== id));
-    } else if (type === 'team') {
-      setTeams(teams.filter(team => team.id !== id));
-    }
-  };
+  const teamsFile = await fs.readFile(process.cwd() + '/lib/data/teams.json', 'utf8');
+  const teamsReq = JSON.parse(teamsFile);
+  const teams = teamsReq.teams;
 
   return (
     <aside className={classes.aside}>
       <>
         <div className={classes['pinned-section']}>
-          <h2>Pinned Leagues</h2>
+          <h2>Popular Leagues</h2>
           {leagues.map(league => (
             <PinnedItem
               key={league.id}
               id={league.id}
               name={league.name}
-              emblem={league.flag}
+              emblem={league.emblem}
               type='league'
-              onUnpin={handlePinClick}
               link={`/league/${league.id}`}
             />
           ))}
         </div>
         <div className={classes['pinned-section']}>
-          <h2>Pinned Teams</h2>
+          <h2>Popular Teams</h2>
           {teams.map(team => (
             <PinnedItem
               key={team.id}
               id={team.id}
               name={team.name}
-              emblem={team.flag}
+              emblem={team.crest}
               type='team'
-              onUnpin={handlePinClick}
               link={`/team/${team.id}`}
             />
           ))}
